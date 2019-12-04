d30e3d0511be2df97455818e3267e17857b87f7b
Ajay Shukla <ajay.shukla@zensar.com>
2019-12-04 18:05:41 +0530

    test fail fix

--- src/test/java/com/learn/MicroServicesCloudApplicationTests.java
+++ src/test/java/com/learn/MicroServicesCloudApplicationTests.java
@@ -1,16 +1,13 @@
 package com.learn;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith( SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class MicroServicesCloudApplicationTests {
 
